9f57a8d6f4e76fea194f14cc080f7307710a7718
Brandon High <brandon.high@puppet.com>
2018-11-01 13:01:42 -0700

    Improve handling when API endpoint is down

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -5,12 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  api_meme_req = requests.get('http://localhost:9001/v1/meme')
-  if api_meme_req.status_code == 200:
-    api_meme = api_meme_req.json()
-    return '<h1>' + api_meme['content'] + '</h1>'
-  else:
-    return 'API is down'
+  api_url = 'http://localhost:9001/v1/meme'
+  try: 
+    api_meme_req = requests.get(api_url, timeout=1)
+    if api_meme_req.status_code == 200:
+      api_meme = api_meme_req.json()
+      return '<h1>' + api_meme['content'] + '</h1>'
+    else:
+      return 'API at ' + api_url + ' returned error code: ' + api_meme_req.status_code
+  except requests.exceptions.ConnectionError:
+    return 'API at ' + api_url + ' failed to connect.'
 
 if __name__ == "__main__":
   app.run()
