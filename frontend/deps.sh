c8dd573b69b66814e6451a9741ff1d26243f6577
Brandon High <brandon.high@puppet.com>
2018-11-01 13:01:09 -0700

    Create Dockerfiles, remove old runscripts

--- frontend/deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-python3 -m venv ./venv
-. ./venv/bin/activate 
-pip install -r requirements.txt
--- frontend/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-port="9000"
-
-. ./venv/bin/activate 
-FLASK_APP="frontend.py" flask run --port="${port}" $@
